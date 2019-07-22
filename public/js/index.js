893f8547c270c1ffe1c5a0b74a3f2c9feb43e6be
LeugimAtreides <m.villarreal789@hotmail.com>
2019-07-21 21:53:12 -0400

    finishing touches of homepage with beginning logic for DOM insertion of photos

--- public/js/index.js
+++ public/js/index.js
@@ -1,99 +1,32 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+$(document).on("ready", function(){
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
+  // photo container holds all the requests
+  var photoContainer = $(".photo-container");
+  
+  // these variables will help shorten the ajaj call
+  var search = $("#search");
+  var query = $("#search");
+  var key = "b0a2431dd332a0620c24ca6ecb88732b";
+  var queryURL = "http://www.flickr.com/services/rest/?api_key=" + key + "&predicate=outerspace&method=flickr.photos.search&per_page=25&tags=outer space&tag_mode=any&text=" + query + "&format=json&extras=url_m"
+
+  search.on("submit", function(){
+
+    // the 'get' method here will display an empty image div with an apology to the user for not bringing back any photos, or bring back all of the photos
+    $.get(queryURL, function(data) {
+      console.log("Photos", data);
+      photos = data;
+      if (!photos || !photos.length) {
+        displayEmpty(query)
+      } else {
+        initializeRows();
+      }
+      
+    })
+
+    
+  })
+  // this gives functionality if no search results are returned
+  function displayEmpty(query){
+    photoContainer.empty()
   }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+})
